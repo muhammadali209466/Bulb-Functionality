80a7a6d101886636ceb995dbdfd48d00381e0fd2
muhammadali209466 <muhammadaligamer209@gmail.com>
2025-05-26 16:44:14 +0500

    Responsive Design

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 function bulbFunctionality(bulbBtn) {
   var imgHandler = document.getElementById("imgHandler");
-  if (bulbBtn.innerHTML === "Bulb On") {
-    imgHandler.height = "300";
+  if (bulbBtn.innerHTML.trim() === "Bulb On") {
+    imgHandler.height = "250";
     imgHandler.src = "./images/bulb on.jpg";
-
     bulbBtn.innerHTML = "Bulb Off";
   } else {
     imgHandler.src = "./images/bulb off.png";
